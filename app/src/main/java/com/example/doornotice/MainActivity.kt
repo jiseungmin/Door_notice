45cad437bfd2c001c206d77b91d2ca46aa2e57b1
test <sm46384638@naver.com>
2023-05-16 09:30:46 +0900

    Button funtion

--- app/src/main/java/com/example/doornotice/MainActivity.kt
+++ app/src/main/java/com/example/doornotice/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.doornotice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import com.example.doornotice.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
@@ -15,17 +17,127 @@ import java.lang.reflect.Type
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private lateinit var snackbar : Snackbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        retrofitevent("1","51")
-
+        snackbar = Snackbar.make(binding.root, "현재 상태를 변경하였습니다.", Snackbar.LENGTH_LONG)
+        Button()
     }
 
-    private fun retrofitevent(id :String,state:String){
+    private fun Button() {
+        binding.IndoorButton.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("상태 확인")
+            builder.setMessage("현재 상태를 재실로 변경하시겠습니까?")
+            builder.setPositiveButton("네") { dialog, which ->
+                retrofitevent("1", "1")
+                snackbar.show()
+            }
+            builder.setNegativeButton("아니요") { dialog, which -> dialog.dismiss() }
+            builder.create().show()
+        }
+
+        binding.MeetingButton.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("상태 확인")
+            builder.setMessage("현재 상태를 회의로 변경하시겠습니까?")
+            builder.setPositiveButton("네") { dialog, which ->
+                retrofitevent("1", "2")
+                snackbar.show()
+            }
+            builder.setNegativeButton("아니요") { dialog, which -> dialog.dismiss() }
+            builder.create().show()
+        }
+
+        binding.LectureButton.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("상태 확인")
+            builder.setMessage("현재 상태를 수업으로 변경하시겠습니까?")
+            builder.setPositiveButton("네") { dialog, which ->
+                retrofitevent("1", "3")
+                snackbar.show()
+            }
+            builder.setNegativeButton("아니요") { dialog, which -> dialog.dismiss() }
+            builder.create().show()
+        }
+
+        binding.EatButton.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("상태 확인")
+            builder.setMessage("현재 상태를 식사로 변경하시겠습니까?")
+            builder.setPositiveButton("네") { dialog, which ->
+                retrofitevent("1", "4")
+                snackbar.show()
+            }
+            builder.setNegativeButton("아니요") { dialog, which -> dialog.dismiss() }
+            builder.create().show()
+        }
+
+        binding.BusinessTripButton.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("상태 확인")
+            builder.setMessage("현재 상태를 출장으로 변경하시겠습니까?")
+            builder.setPositiveButton("네") { dialog, which ->
+                retrofitevent("1", "5")
+                snackbar.show()
+            }
+            builder.setNegativeButton("아니요") { dialog, which -> dialog.dismiss() }
+            builder.create().show()
+        }
+
+        binding.OutingButton.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("상태 확인")
+            builder.setMessage("현재 상태를 외출로 변경하시겠습니까?")
+            builder.setPositiveButton("네") { dialog, which ->
+                retrofitevent("1", "6")
+                snackbar.show()
+            }
+            builder.setNegativeButton("아니요") { dialog, which -> dialog.dismiss() }
+            builder.create().show()
+        }
+
+        binding.ConsultingButton.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("상태 확인")
+            builder.setMessage("현재 상태를 학생 상담으로 변경하시겠습니까?")
+            builder.setPositiveButton("네") { dialog, which ->
+                retrofitevent("1", "7")
+                snackbar.show()
+            }
+            builder.setNegativeButton("아니요") { dialog, which -> dialog.dismiss() }
+            builder.create().show()
+        }
+
+        binding.GyonaeButton.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("상태 확인")
+            builder.setMessage("현재 상태를 교내 외출로 변경하시겠습니까?")
+            builder.setPositiveButton("네") { dialog, which ->
+                retrofitevent("1", "8")
+                snackbar.show()
+            }
+            builder.setNegativeButton("아니요") { dialog, which -> dialog.dismiss() }
+            builder.create().show()
+        }
+
+        binding.LeaveWorkButton.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("상태 확인")
+            builder.setMessage("현재 상태를 퇴근으로 변경하시겠습니까?")
+            builder.setPositiveButton("네") { dialog, which ->
+                retrofitevent("1", "9")
+                snackbar.show()
+            }
+            builder.setNegativeButton("아니요") { dialog, which -> dialog.dismiss() }
+            builder.create().show()
+        }
+    }
+
+    private fun retrofitevent(id :String, state:String){
          val nullOnEmptyConverterFactory = object : Converter.Factory() {
             fun converterFactory() = this
             override fun responseBodyConverter(type: Type, annotations: Array<out Annotation>, retrofit: Retrofit) = object : Converter<ResponseBody, Any?> {
--- app/src/main/java/com/example/doornotice/Qwe.kt
+++
@@ -1,3 +0,0 @@
-package com.example.doornotice
-
-class Qwe : ArrayList<User>()
